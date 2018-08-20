5ba7f5e619b7c68d7251ab24ac7498b326bbf390
Jay Escala <Jonathan.J.Escala@gmail.com>
2018-08-20 11:02:25 -0500

    Set up intial files for React.

---
+++ src/HomeContainer/HomeContainer.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class HomeContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      model: []
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Home Page</h1>
+      </div>
+    )
+  }
+}
+
+export default HomeContainer;
